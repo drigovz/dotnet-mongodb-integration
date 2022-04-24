b8869180d4d27195ff7ab6114856d65f0158ebca
Rodrigo Vaz <rodrigodp2014@gmail.com>
2022-04-23 22:05:58 -0300

    feat: Add get all method

--- src/MongoDbIntegration.Api/Controllers/ProductsController.cs
+++ src/MongoDbIntegration.Api/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
-﻿using MongoDbIntegration.Application.Core.Products.Commands;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using MongoDbIntegration.Application.Core.Products.Commands;
+using MongoDbIntegration.Application.Core.Products.Queries;
 
 namespace MongoDbIntegration.Api.Controllers
 {
@@ -18,19 +19,19 @@ namespace MongoDbIntegration.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] CreateProductCommand request)
-            => Ok(await _mediator.Send(request));
+        public async Task<IActionResult> Create([FromBody] CreateProductCommand request) =>
+            Ok(await _mediator.Send(request));
 
         [HttpDelete]
-        public async Task<IActionResult> Remove([FromBody] RemoveProductCommand request)
-            => Ok(await _mediator.Send(request));
+        public async Task<IActionResult> Remove([FromBody] RemoveProductCommand request) =>
+            Ok(await _mediator.Send(request));
 
         [HttpPatch]
-        public async Task<IActionResult> Update([FromBody] UpdateProductCommand request)
-        {
-            HttpContext.Response.ContentType = "application/json";
-            HttpContext.Response.StatusCode = 200; 
-            return Ok(await _mediator.Send(request));
-        }
+        public async Task<IActionResult> Update([FromBody] UpdateProductCommand request) =>
+            Ok(await _mediator.Send(request));
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+            Ok(await _mediator.Send(new ListProductsQuery()));
     }
 }
